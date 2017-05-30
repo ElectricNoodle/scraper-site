40fe6dcfba0c6463be45882e8d4b06da1a8ef75b
Steven Crangle <electricnoodle7@googlemail.com>
2017-05-30 07:44:01 +0000

    Added login stuff, still need to fix a few dead links/bugs.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, json, request
 from werkzeug import generate_password_hash, check_password_hash
 from flaskext.mysql import MySQL
+from flask import session
 
 app = Flask(__name__)
+app.secret_key = 'squirrels like to play in the park.'
 
 mysql = MySQL()
  
@@ -23,6 +25,40 @@ def main():
 @app.route('/showSignin')
 def showSignin():
     return render_template('sign_in.html')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+ 
+ 
+ 
+        # connect to mysql
+ 
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('validateLogin',(_username,))
+        data = cursor.fetchall()
+ 
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+ 
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+
+ 
+
 
 @app.route('/showSignUp')
 def showSignUp():
