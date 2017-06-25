86d059ab3423650c368ac80fcd5af1d10ce2bbef
Steven Crangle <electricnoodle7@googlemail.com>
2017-06-25 17:45:34 +0000

    Added item display and fixed bugs in adding item.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, redirect,session
+from flask import Flask, render_template, json, request, redirect,session,jsonify
 from werkzeug import generate_password_hash, check_password_hash
 from flaskext.mysql import MySQL
 from flask import session
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = 'squirrels like to play in the park.'
 
 mysql = MySQL()
- 
+#SAUSAGE
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'scraper'
 app.config['MYSQL_DATABASE_PASSWORD'] = '42Ir&fdds'
@@ -31,13 +31,21 @@ def showSignin():
 @app.route('/home')
 def userHome():
     if session.get('user'):
-        return render_template('user_home.html')
+        con = mysql.connect()
+        cursor = con.cursor()
+        print session['email'];
+        cursor.callproc('getItems',(session['email'],))
+        entries = [dict(id=row[0], user=row[1],name=row[2], url=row[3], price= row[4],shipping= row[5],stock_no= row[6],status=row[7], last_updated = row[8], regex= row[9]) for row in cursor.fetchall()]
+        print entries;
+        return render_template('user_home.html',items = entries)
     else:
         return render_template('error.html',error = 'Unauthorized Access')
+
 
 @app.route('/logout')
 def logout():
     session.pop('user',None)
+    session.pop('email',None)
     return redirect('/')
 
 @app.route('/validateLogin',methods=['POST'])
@@ -58,6 +66,7 @@ def validateLogin():
         if len(data) > 0:
             if check_password_hash(str(data[0][3]),_password):
                 session['user'] = data[0][0]
+                session['email'] = _username
                 return redirect('/home')
             else:
                 return render_template('error.html',error = 'Wrong Email address or Password.')
@@ -107,13 +116,26 @@ def showAddItem():
 def addItem():
     url = request.form['inputUrl']
 
-    urlCheck = re.search( r'[www.|\s?]([a-z]+)\.[com|co\.uk]+', url, re.M|re.I)
+    urlCheck = re.search( r'[www.|\s?]([a-z]+)\.[com|co\.uk]+\/([\%0-9a-zA-Z-]+)', url, re.M|re.I)
 
     if urlCheck:
        print "searchObj.group() : ", urlCheck.group()
        print "searchObj.group(1) : ", urlCheck.group(1)
+       print "searchObj.group(2) :",  urlCheck.group(2)
+       item = urlCheck.group(2)
+       if urlCheck.group(1) == 'wayfair':
+           cursor.callproc('addItem',(url,session['email'], item))
+           data = cursor.fetchall()
+           if len(data) is 0:
+               conn.commit()
+               result = "Added: " + url
+               return render_template('add_item.html', result = result, success = 1)
+       else:
+            result = "Only wayfair is supported for now."
+            return render_template('add_item.html', result = result, success = 0)
     else:
-       return render_template('error.html',error = url + ' is not a valid URL :(.')
+       result = "Error adding link :("
+       return render_template('add_item.html', result = result, success = 0)
 
 if __name__ == "__main__":
     app.run(host='10.8.46.3')
